becd0140909b590576e93bc9498d1bbd2e28493c
Spike <florian@playtoday.cc>
2023-10-05 22:33:56 +1100

    feat: zod installation + grabbing form data

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import {View, Text, TouchableOpacity, TextInput} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Animated, {
   Easing,
   FadeInUp,
@@ -13,7 +13,21 @@ import Animated, {
   ReduceMotion,
 } from 'react-native-reanimated';
 
+import {z} from 'zod';
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type FormErrors = {
+  email?: string;
+  password?: string;
+};
+
 const LoginScreen = () => {
+  const [formData, setFormData] = useState({email: '', password: ''});
+  const [formErrors, setFormErrors] = useState<FormErrors>({});
   const rocketTranslateY = useSharedValue(120); // Initial position outside the screen
   const rocketTranslateX = useSharedValue(-280); // Initial position outside the screen
   const rocketScale = useSharedValue(0.1); // Initial small size
@@ -60,6 +74,23 @@ const LoginScreen = () => {
     };
   });
 
+  const handleSubmit = () => {
+    try {
+      const validatedData = loginSchema.parse(formData);
+      console.log(validatedData);
+      setFormErrors({});
+    } catch (error) {
+      const zodError = error as z.ZodError;
+      const newFormErrors: FormErrors = {};
+      zodError.errors.forEach(err => {
+        if (err.path[0]) {
+          newFormErrors[err.path[0] as keyof FormErrors] = err.message;
+        }
+      });
+      setFormErrors(newFormErrors);
+    }
+  };
+
   return (
     <View className="bg-white h-full w-full">
       <Animated.Image
@@ -89,14 +120,23 @@ const LoginScreen = () => {
         <Animated.View
           entering={FadeInDown.delay(200).duration(1000).springify()}
           className="bg-black/5 p-5 rounded-2xl w-full">
-          <TextInput placeholder="Email" placeholderTextColor={'gray'} />
+          <TextInput
+            className="text-xl"
+            placeholder="Email"
+            placeholderTextColor={'gray'}
+            value={formData.email}
+            onChangeText={text => setFormData({...formData, email: text})}
+          />
         </Animated.View>
         <Animated.View
           entering={FadeInDown.delay(400).duration(1000).springify()}
           className="bg-black/5 p-5 rounded-2xl w-full mb-3">
           <TextInput
+            className="text-xl"
             placeholder="Password"
             placeholderTextColor={'gray'}
+            value={formData.password}
+            onChangeText={text => setFormData({...formData, password: text})}
             secureTextEntry
           />
         </Animated.View>
@@ -104,7 +144,9 @@ const LoginScreen = () => {
         <Animated.View
           className="w-full"
           entering={FadeInDown.delay(600).duration(1000).springify()}>
-          <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
+          <TouchableOpacity
+            className="w-full bg-sky-400 p-3 rounded-2xl mb-3"
+            onPress={handleSubmit}>
             <Text className="text-xl font-bold text-white text-center">
               Login
             </Text>
